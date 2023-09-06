a6809b487e56321ebc3511c68df2abd64c192d2a
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2023-09-06 15:35:12 +0900

    Update data_prep_main.py

--- AUTOML/data_prep_main.py
+++ AUTOML/data_prep_main.py
@@ -7,19 +7,28 @@ Original file is located at
     https://colab.research.google.com/drive/19NeFY4LdUE0JCQGcMN8ukYgiZvp6yS8x
 """
 
-from mol_features import data_prep
-from maccs_features import generate_and_concatenate_MACCS_keys
 import pandas as pd
 
-train = pd.read_csv('/train_ms.csv')
-test = pd.read_csv('/test_ms.csv')
-
-train.loc[2796, 'AlogP'] = train.loc[2796, 'LogD']
-train.loc[3387, 'AlogP'] = train.loc[3387, 'LogD']
-test.loc[10, 'AlogP'] = test.loc[10, 'LogD']
-
-trainf = data_prep(train)
-testf = data_prep(test)
-
-generate_and_concatenate_MACCS_keys(trainf, drop_columns=['HLM'])
-generate_and_concatenate_MACCS_keys(testf)
+def preprocess_data():
+    """
+    Loads and preprocesses the train and test data.
+    
+    Returns:
+    - train: Preprocessed training dataframe.
+    - test: Preprocessed testing dataframe.
+    """
+    
+    # Paths (assuming these are constants for your project)
+    train_data_path = '/train_ms.csv'
+    test_data_path = '/test_ms.csv'
+    
+    # Load data
+    train = pd.read_csv(train_data_path)
+    test = pd.read_csv(test_data_path)
+    
+    # Preprocess data
+    train.loc[2796, 'AlogP'] = train.loc[2796, 'LogD']
+    train.loc[3387, 'AlogP'] = train.loc[3387, 'LogD']
+    test.loc[10, 'AlogP'] = test.loc[10, 'LogD']
+    
+    return train, test

ad0c90576cb7bd484a9bb8121da2fc82c2964e96
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2023-09-13 11:39:45 +0900

    main

--- AUTOML/main.py
+++ AUTOML/main.py
@@ -5,6 +5,7 @@ from data_prep_main import preprocess_call_data
 from morgan_finger_features import generate_morgan_fingerprints_and_concat
 from maccs_features import generate_and_concatenate_MACCS_keys
 from mol_features import data_prep
+from similarity_features import compute_similarity, add_similarity_features
 from autogluon.tabular import TabularPredictor
 
 def main(args):

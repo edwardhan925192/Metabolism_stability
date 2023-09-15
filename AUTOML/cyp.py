784a6a2da0bd1b941e5473a26864aec9713e6e4c
edwardhan925151 <127165920+edwardhan925192@users.noreply.github.com>
2023-09-16 08:56:06 +0900

    Update cyp.py

--- AUTOML/cyp.py
+++ AUTOML/cyp.py
@@ -3,6 +3,6 @@ import pandas as pd
 def cyp_concat(train, test, train_path, test_path):    
     swiss1 = pd.read_csv(train_path)
     swiss2 = pd.read_csv(test_path)
-    concat_df1 = pd.concat([train, swiss1], ignore_index=True)
-    concat_df2 = pd.concat([test, swiss2], ignore_index=True)
+    concat_df1 = pd.concat([train, swiss1],axis =1)
+    concat_df2 = pd.concat([test, swiss2],axis =1)
     return concat_df1, concat_df2
